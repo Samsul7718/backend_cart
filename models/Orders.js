b6ac52cd2e4188b36b3efb5a05521b22c12c4333
samsul alam <samsul7718@gmail.com>
2025-11-13 20:46:33 +0530

    order.js created

---
+++ models/Orders.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const orderSchema=new mongoose.Schema({
+    orderId:{type:String,required:true},
+    paymentId:{type:String},
+    signature:{type:String},
+    amount:{type:Number,required:true},
+    currency:{type:String,default:"INR"},
+    status:{type:String,default:"created"},
+     cartItems: [                                 
+    {
+      productId: String,
+      title: String,
+      price: Number,
+      qty: Number,
+    }
+  ],
+   createdAt: { type: Date, default: Date.now },
+})
+
+export default mongoose.model("Order",orderSchema);
